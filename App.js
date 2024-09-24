3d98932120e9879314c24e9ec1a6bfb2b5038d95
Zaibhere1 <zaibchaudhary4492@gmail.com~>
2024-09-24 15:23:15 +0500

    first commit

--- App.js
+++ App.js
@@ -1,145 +1,17 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
-const port = 3000; // You can choose any port
-const logos = [
-  {
-    name: "3M",
-    url: "./logos/3m.svg",
-  },
-  {
-    name: "Barstool Store",
-    url: "./logos/barstool-store.svg",
-  },
-  {
-    name: "Budweiser",
-    url: "./logos/budweiser.svg",
-  },
-  {
-    name: "BuzzFeed",
-    url: "./logos/buzzfeed.svg",
-  },
-  {
-    name: "Forbes",
-    url: "./logos/forbes.svg",
-  },
-  {
-    name: "Macy's",
-    url: "./logos/macys.svg",
-  },
-  {
-    name: "Men's Health",
-    url: "./logos/menshealth.svg",
-  },
-  {
-    name: "MrBeast",
-    url: "./logos/mrbeast.svg",
-  },
-  {
-    name: "3M",
-    url: "./logos/3m.svg",
-  },
-  {
-    name: "Barstool Store",
-    url: "./logos/barstool-store.svg",
-  },
-  {
-    name: "Budweiser",
-    url: "./logos/budweiser.svg",
-  },
-  {
-    name: "BuzzFeed",
-    url: "./logos/buzzfeed.svg",
-  },
-  {
-    name: "Forbes",
-    url: "./logos/forbes.svg",
-  },
-  {
-    name: "Macy's",
-    url: "./logos/macys.svg",
-  },
-  {
-    name: "Men's Health",
-    url: "./logos/menshealth.svg",
-  },
-  {
-    name: "MrBeast",
-    url: "./logos/mrbeast.svg",
-  },
-  {
-    name: "3M",
-    url: "./logos/3m.svg",
-  },
-  {
-    name: "Barstool Store",
-    url: "./logos/barstool-store.svg",
-  },
-  {
-    name: "Budweiser",
-    url: "./logos/budweiser.svg",
-  },
-  {
-    name: "BuzzFeed",
-    url: "./logos/buzzfeed.svg",
-  },
-  {
-    name: "Forbes",
-    url: "./logos/forbes.svg",
-  },
-  {
-    name: "Macy's",
-    url: "./logos/macys.svg",
-  },
-  {
-    name: "Men's Health",
-    url: "./logos/menshealth.svg",
-  },
-  {
-    name: "MrBeast",
-    url: "./logos/mrbeast.svg",
-  },
-  {
-    name: "3M",
-    url: "./logos/3m.svg",
-  },
-  {
-    name: "Barstool Store",
-    url: "./logos/barstool-store.svg",
-  },
-  {
-    name: "Budweiser",
-    url: "./logos/budweiser.svg",
-  },
-  {
-    name: "BuzzFeed",
-    url: "./logos/buzzfeed.svg",
-  },
-  {
-    name: "Forbes",
-    url: "./logos/forbes.svg",
-  },
-  {
-    name: "Macy's",
-    url: "./logos/macys.svg",
-  },
-  {
-    name: "Men's Health",
-    url: "./logos/menshealth.svg",
-  },
-  {
-    name: "MrBeast",
-    url: "./logos/mrbeast.svg",
-  },
-];
+const port = 3000;
+
 
 app.use(express.json());
 
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Express.js!", logos });
+  res.json("Hello from Express.js!");
 });
 
 
-app.listen(port, () => {
-  console.log("Server listening on port  http://localhost:3000/");
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port  http://localhost:${port}/`);
 });
