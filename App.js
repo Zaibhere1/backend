0643f028a79f6d5f1d410e399e6a1c3f09348f3a
Zaibhere1 <zaibchaudhary4492@gmail.com~>
2024-10-15 13:02:50 +0500

    f

--- App.js
+++ App.js
@@ -1,17 +1,347 @@
 require('dotenv').config()
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const cors = require('cors')
 
-
+app.use(cors());
 app.use(express.json());
 
-
+app.use(express.static("dist"));
 app.get("/", (req, res) => {
   res.json("Hello from Express.js!");
 });
 
+const logos = [
+  {
+    name: "3M",
+    url: "./logos/3m.svg",
+  },
+  {
+    name: "Barstool Store",
+    url: "./logos/barstool-store.svg",
+  },
+  {
+    name: "Budweiser",
+    url: "./logos/budweiser.svg",
+  },
+  {
+    name: "BuzzFeed",
+    url: "./logos/buzzfeed.svg",
+  },
+  {
+    name: "Forbes",
+    url: "./logos/forbes.svg",
+  },
+  {
+    name: "Macy's",
+    url: "./logos/macys.svg",
+  },
+  {
+    name: "Men's Health",
+    url: "./logos/menshealth.svg",
+  },
+  {
+    name: "MrBeast",
+    url: "./logos/mrbeast.svg",
+  },
+  {
+    name: "3M",
+    url: "./logos/3m.svg",
+  },
+  {
+    name: "Barstool Store",
+    url: "./logos/barstool-store.svg",
+  },
+  {
+    name: "Budweiser",
+    url: "./logos/budweiser.svg",
+  },
+  {
+    name: "BuzzFeed",
+    url: "./logos/buzzfeed.svg",
+  },
+  {
+    name: "Forbes",
+    url: "./logos/forbes.svg",
+  },
+  {
+    name: "Macy's",
+    url: "./logos/macys.svg",
+  },
+  {
+    name: "Men's Health",
+    url: "./logos/menshealth.svg",
+  },
+  {
+    name: "MrBeast",
+    url: "./logos/mrbeast.svg",
+  },
+  {
+    name: "3M",
+    url: "./logos/3m.svg",
+  },
+  {
+    name: "Barstool Store",
+    url: "./logos/barstool-store.svg",
+  },
+  {
+    name: "Budweiser",
+    url: "./logos/budweiser.svg",
+  },
+  {
+    name: "BuzzFeed",
+    url: "./logos/buzzfeed.svg",
+  },
+  {
+    name: "Forbes",
+    url: "./logos/forbes.svg",
+  },
+  {
+    name: "Macy's",
+    url: "./logos/macys.svg",
+  },
+  {
+    name: "Men's Health",
+    url: "./logos/menshealth.svg",
+  },
+  {
+    name: "MrBeast",
+    url: "./logos/mrbeast.svg",
+  },
+  {
+    name: "3M",
+    url: "./logos/3m.svg",
+  },
+  {
+    name: "Barstool Store",
+    url: "./logos/barstool-store.svg",
+  },
+  {
+    name: "Budweiser",
+    url: "./logos/budweiser.svg",
+  },
+  {
+    name: "BuzzFeed",
+    url: "./logos/buzzfeed.svg",
+  },
+  {
+    name: "Forbes",
+    url: "./logos/forbes.svg",
+  },
+  {
+    name: "Macy's",
+    url: "./logos/macys.svg",
+  },
+  {
+    name: "Men's Health",
+    url: "./logos/menshealth.svg",
+  },
+  {
+    name: "MrBeast",
+    url: "./logos/mrbeast.svg",
+  },
+  {
+    name: "3M",
+    url: "./logos/3m.svg",
+  },
+  {
+    name: "Barstool Store",
+    url: "./logos/barstool-store.svg",
+  },
+  {
+    name: "Budweiser",
+    url: "./logos/budweiser.svg",
+  },
+  {
+    name: "BuzzFeed",
+    url: "./logos/buzzfeed.svg",
+  },
+  {
+    name: "Forbes",
+    url: "./logos/forbes.svg",
+  },
+  {
+    name: "Macy's",
+    url: "./logos/macys.svg",
+  },
+  {
+    name: "Men's Health",
+    url: "./logos/menshealth.svg",
+  },
+  {
+    name: "MrBeast",
+    url: "./logos/mrbeast.svg",
+  },
+  {
+    name: "3M",
+    url: "./logos/3m.svg",
+  },
+  {
+    name: "Barstool Store",
+    url: "./logos/barstool-store.svg",
+  },
+  {
+    name: "Budweiser",
+    url: "./logos/budweiser.svg",
+  },
+  {
+    name: "BuzzFeed",
+    url: "./logos/buzzfeed.svg",
+  },
+  {
+    name: "Forbes",
+    url: "./logos/forbes.svg",
+  },
+  {
+    name: "Macy's",
+    url: "./logos/macys.svg",
+  },
+  {
+    name: "Men's Health",
+    url: "./logos/menshealth.svg",
+  },
+  {
+    name: "MrBeast",
+    url: "./logos/mrbeast.svg",
+  },
+  {
+    name: "3M",
+    url: "./logos/3m.svg",
+  },
+  {
+    name: "Barstool Store",
+    url: "./logos/barstool-store.svg",
+  },
+  {
+    name: "Budweiser",
+    url: "./logos/budweiser.svg",
+  },
+  {
+    name: "BuzzFeed",
+    url: "./logos/buzzfeed.svg",
+  },
+  {
+    name: "Forbes",
+    url: "./logos/forbes.svg",
+  },
+  {
+    name: "Macy's",
+    url: "./logos/macys.svg",
+  },
+  {
+    name: "Men's Health",
+    url: "./logos/menshealth.svg",
+  },
+  {
+    name: "MrBeast",
+    url: "./logos/mrbeast.svg",
+  },
+  {
+    name: "3M",
+    url: "./logos/3m.svg",
+  },
+  {
+    name: "Barstool Store",
+    url: "./logos/barstool-store.svg",
+  },
+  {
+    name: "Budweiser",
+    url: "./logos/budweiser.svg",
+  },
+  {
+    name: "BuzzFeed",
+    url: "./logos/buzzfeed.svg",
+  },
+  {
+    name: "Forbes",
+    url: "./logos/forbes.svg",
+  },
+  {
+    name: "Macy's",
+    url: "./logos/macys.svg",
+  },
+  {
+    name: "Men's Health",
+    url: "./logos/menshealth.svg",
+  },
+  {
+    name: "MrBeast",
+    url: "./logos/mrbeast.svg",
+  },
+  {
+    name: "3M",
+    url: "./logos/3m.svg",
+  },
+  {
+    name: "Barstool Store",
+    url: "./logos/barstool-store.svg",
+  },
+  {
+    name: "Budweiser",
+    url: "./logos/budweiser.svg",
+  },
+  {
+    name: "BuzzFeed",
+    url: "./logos/buzzfeed.svg",
+  },
+  {
+    name: "Forbes",
+    url: "./logos/forbes.svg",
+  },
+  {
+    name: "Macy's",
+    url: "./logos/macys.svg",
+  },
+  {
+    name: "Men's Health",
+    url: "./logos/menshealth.svg",
+  },
+  {
+    name: "MrBeast",
+    url: "./logos/mrbeast.svg",
+  },
+  {
+    name: "3M",
+    url: "./logos/3m.svg",
+  },
+  {
+    name: "Barstool Store",
+    url: "./logos/barstool-store.svg",
+  },
+  {
+    name: "Budweiser",
+    url: "./logos/budweiser.svg",
+  },
+  {
+    name: "BuzzFeed",
+    url: "./logos/buzzfeed.svg",
+  },
+  {
+    name: "Forbes",
+    url: "./logos/forbes.svg",
+  },
+  {
+    name: "Macy's",
+    url: "./logos/macys.svg",
+  },
+  {
+    name: "Men's Health",
+    url: "./logos/menshealth.svg",
+  },
+  {
+    name: "MrBeast",
+    url: "./logos/mrbeast.svg",
+  },
+];
+
+app.get('/logos', (req, res) => {
+  res.send(logos);
+
+});
+
+
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server listening on port  http://localhost:${port}/`);
+  console.log(`Server listening on port  http://localhost:${port}`);
 });
---
+++ dist/assets/Gallery-CqxBr0kd.js
@@ -0,0 +1,1 @@
+import{M as o}from"./MainLayout-D1BwbsVN.js";import{e as r}from"./index-DMnC2pVp.js";const e=()=>r(o,{});export{e as default};
